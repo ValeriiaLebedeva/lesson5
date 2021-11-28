3a2ffd21901c47036c8774f4af98478a15eb6f94
Valeriya <lerikalebedeva4@gmail.com>
2021-11-29 02:58:03 +0300

    add CredentialsConfig

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,21 +2,43 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
 
+    public static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
     RegistrationPage registrationPage = new RegistrationPage();
+
+
+//    @Test
+//    void readCredentialsTest() {
+//        String login = credentials.login();
+//        String password = credentials.password();
+
+//        System.out.println(login);
+//        System.out.println(password);
+//        String message = format("i login as %s with password %s", login, password);
+//        System.out.println(message);
+//    }
 
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
+        String value = System.getProperty("url", "https://selenoid.autotests.cloud/");
+        String login = credentials.login();
+        String password = credentials.password();
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
